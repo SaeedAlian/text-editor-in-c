72a1eb6a507d5ddeb965405e4edde5db067391f7
SaeedAlian <aliansaeed82@gmail.com>
2024-08-02 16:40:04 +0330

    init project

---
+++ CMakeFiles/main.o.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.o.dir/main.c.o"
+  "CMakeFiles/main.o.dir/main.c.o.d"
+  "out/main.o"
+  "out/main.o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ run.sh
@@ -0,0 +1,3 @@
+cmake .
+make
+./out/main.o
